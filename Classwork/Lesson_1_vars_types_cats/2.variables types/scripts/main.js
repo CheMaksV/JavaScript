d877d94413318314282b35b5bee8792c97a3b534
CheMaksV <cheredilin.biz@gmail.com>
2025-07-02 00:16:55 +0300

    add 2.variables types add  index.html, main.js

---
+++ Classwork/Lesson_1_vars_types_cats/2.variables types/scripts/main.js
@@ -0,0 +1,28 @@
+// console.log('qweqweqwer');
+
+//number
+//string
+//boolean
+
+// var a;
+// a = 'hello okten';
+
+// var a= 'hello okten';
+
+
+// {
+//     let a = 'hello okten';
+//     var b = 'hello world';
+//
+// }
+// console.log(b);
+// console.log(a);
+
+const PI= 3.14;
+console.log(PI);
+// PI = 123;
+
+let a = 'asdasdasdasd';
+console.log(a);
+a = 'okten';
+console.log(a);
