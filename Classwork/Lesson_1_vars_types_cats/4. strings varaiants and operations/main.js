228647a427668fc66ff97b75cd37c8c6f95cbf99
CheMaksV <cheredilin.biz@gmail.com>
2025-07-08 16:54:36 +0300

    add 4. strings varaiants and operations add  index.html, main.js

---
+++ Classwork/Lesson_1_vars_types_cats/4. strings varaiants and operations/main.js
@@ -0,0 +1,14 @@
+let a = "'asd'";
+let b = '"asd"';
+console.log(a, b);
+
+let c = `asd`;
+console.log(c);
+
+let greeting = 'hello';
+let intro = 'my name is Vasya';
+
+let msg = greeting + ' ' + intro;
+console.log(msg);
+let msg2 = `${greeting} ${intro}`;
+console.log(msg2);
