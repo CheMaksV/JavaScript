32511178ec7c7210b0fb0039d6230d21e49765a4
CheMaksV <cheredilin.biz@gmail.com>
2025-07-17 00:18:16 +0300

    add 5. array of obj add index.html, main.js

---
+++ Classwork/Lesson_2_arrays_objects_if/5. array of obj/main.js
@@ -0,0 +1,21 @@
+let users = [
+    {skills: ['html', 'mysql', 'mongo'], id: 1, name: 'vasya', age: 31, status: true},
+    {skills: ['html', 'js'], id: 2, name: 'petya', age: 33, status: false},
+    {skills: ['html', 'js'], id: 3, name: 'anna', age: 28, status: true},
+    {skills: ['html', 'mysql', 'mongo'], id: 4, name: 'olya', age: 29, status: false}
+];
+console.log(users);
+console.log(users[0]);
+let user0 = users[0];
+console.log(user0.age);
+console.log(user0['age']);
+
+console.log(users[0]);
+console.log(users[0].skills);
+console.log(users[0].skills[2]);
+console.log(users[0]['skills'][2]);
+let user = users[0];
+let skills = user['skills'];
+console.log(skills[2]);
+
+
