95e54a579f08584ab2cbe7ac0c0444fa740f7306
CheMaksV <cheredilin.biz@gmail.com>
2025-07-19 20:49:04 +0300

    add 10. ternary add index.html, main.js

---
+++ Classwork/Lesson_2_arrays_objects_if/10. ternary/main.js
@@ -0,0 +1,18 @@
+let obj = {
+    id: 1,
+    name: "vasya",
+    age: 19
+}
+
+// let access;
+// // let condition = obj.age > 18;
+// if (obj.age > 18) {
+//     access = true;
+// } else {
+//     access = false;
+// }
+// console.log(access);
+
+// let access = obj.age > 18 ? true : false;
+let access = obj.age > 18 ? 'Yes' : 'No';
+console.log(access);
