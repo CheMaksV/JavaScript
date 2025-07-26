8ffff2dc49d0a0019cb58e447b277cd977d9c752
CheMaksV <cheredilin.biz@gmail.com>
2025-07-27 00:18:48 +0300

    add additional1_11-15 add index.html, main.js

---
+++ Homework/hw3/additional/additional1/additional1_11-15/main.js
@@ -0,0 +1,46 @@
+//  #mDMWMW5a
+// - Створити цикл for на 10 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    document.write(`${i} `)
+}
+console.log('');
+document.write(`<br>`)
+
+// #4sXhaa5YMM
+// - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+    document.write(`${i} `)
+}
+console.log('');
+document.write(`<br>`)
+
+// #s24slNyz7
+// - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+for (let i = 0; i < 100; i += 2) {
+    console.log(i);
+    document.write(`${i} `)
+}
+console.log('');
+document.write(`<br>`)
+
+// #zananT5FR1
+// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+        document.write(`${i} `)
+    }
+}
+console.log('');
+document.write(`<br>`)
+
+// #Tfrwls7FM
+// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+for (let i = 0; i < 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+        document.write(`${i} `)
+    }
+}
