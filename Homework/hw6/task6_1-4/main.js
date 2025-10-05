cd1da1b5eddb986869945cb6b16206a9f17a8a6a
CheMaksV <cheredilin.biz@gmail.com>
2025-10-05 21:58:12 +0300

    add hw6, task6_1-4 add index.html, main.js

---
+++ Homework/hw6/task6_1-4/main.js
@@ -0,0 +1,38 @@
+// #dFeorS3m7u
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+let str1 = 'hello world';
+console.log(str1.length);
+let str2 = 'lorem ipsum';
+console.log(str2.length);
+let str3 = 'javascript is cool';
+console.log(str3.length);
+
+
+// #8lld9HMxXWB
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+let str4 = 'hello world';
+console.log(str4.toUpperCase());
+let str5 = 'lorem ipsum';
+console.log(str5.toUpperCase());
+let str6 = 'javascript is cool';
+console.log(str6.toUpperCase());
+
+
+// #ClDsAm7xba7
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+let str7 = 'HELLO WORLD';
+console.log(str7.toLowerCase());
+let str8 = 'LOREM IPSUM';
+console.log(str8.toLowerCase());
+let str9 = 'JAVASCRIPT IS COOL';
+console.log(str9.toLowerCase());
+
+
+// #0b89BkYZwu
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+let str = ' dirty string   '
+let s = str.trim();
+console.log(s);
