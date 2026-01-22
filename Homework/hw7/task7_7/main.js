56774d87b774108b32e984709d2264d1d0cbea09
CheMaksV <cheredilin.biz@gmail.com>
2026-01-22 22:00:43 +0200

    add hw7, task7_7 add index.html, main.js

---
+++ Homework/hw7/task7_7/main.js
@@ -0,0 +1,50 @@
+// #5kla3yMpgp
+// – (Те саме, тільки через клас)
+// Створити клас, який дозволяє створювати об’єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об’єм двигуна. додати в об’єкт функції:
+//     — drive () – яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+//     — info () – яка виводить всю інформацію про автомобіль в форматі `назва поля – значення поля`
+//     — increaseMaxSpeed (newSpeed) – яка підвищує значення максимальної швидкості на значення newSpeed
+//     — changeYear (newValue) – змінює рік випуску на значення newValue
+//     — addDriver (driver) – приймає об’єкт, який “водій” з довільним набором полів, і додає його в поточний об’єкт car
+
+class Car {
+    constructor(model, producer, yearOfRelease, maxSpeed, engineCapacity) {
+        this.model = model;
+        this.producer = producer;
+        this.yearOfRelease = yearOfRelease;
+        this.maxSpeed = maxSpeed;
+        this.engineCapacity = engineCapacity;
+    }
+
+    drive() {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+    }
+
+
+    info() {
+        for (let key in this) {
+            console.log(key, this[key]);
+        }
+    }
+
+    increaseMaxSpeed(newSpeed) {
+        if (newSpeed > 0) this.maxSpeed += newSpeed;
+    }
+
+    changeYear(newYear) {
+        if (newYear > 1800) this.yearOfRelease = newYear;
+    }
+
+    addDriver(driverObject) {
+        if (driverObject) this.driver = driverObject;
+    }
+}
+
+let car = new Car('Tesla', 'Tesla Motors', 2012, 250, 8);
+console.log(car);
+car.drive();
+car.info();
+car.increaseMaxSpeed(50);
+car.changeYear(2015)
+car.addDriver({name: 'vasya', age: 25});
+console.log(car);
